0cbb983b1682728d9104a1a1f69ea2fe89bb1358
Centeno Jose Antonio <jocecito182@gmail.com>
2021-09-28 22:41:33 -0300

    Last Commit?

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("Express");
+const mongoose = require("mongoose");
 const ejs = require("ejs");
 const app = new express();
 
@@ -8,7 +9,29 @@ app.use(express.urlencoded());
 
 app.set("view engine", "ejs");
 
-let items = [];
+mongoose.connect("mongodb+srv://todolistadmin:Test123@cluster0.upsvt.mongodb.net/todolistDB", { useNewUrlParser: true });
+
+const itemsSchemma = {
+    name: String
+};
+
+const Item = mongoose.model("Item", itemsSchemma);
+
+const item1 = new Item({
+    name: "Welcome to your todolist!"
+});
+
+const item2 = new Item({
+    name: "Hit the + button to add a new item."
+});
+
+const item3 = new Item({
+    name: "<-- hit this to delete an item."
+});
+
+const defaultItems = [item1, item2, item3]
+
+
 
 app.get("/", function(req, res) {
 
@@ -24,14 +47,41 @@ app.get("/", function(req, res) {
 
     let day = today.toLocaleDateString("en-US", options);
 
-    res.render("list", { listTitle: day, itemList: items });
+    Item.find({}, (e, foundItems) => {
+        if (foundItems.length === 0) {
+            Item.insertMany(defaultItems, e => {
+                if (e) {
+                    console.log(e);
+                } else {
+                    console.log("Good job! items has been added!")
+                }
+            })
+            res.redirect("/");
+        } else {
+            res.render("list", { listTitle: day, itemList: foundItems });
+        }
+    });
+
 
 });
 
 app.post("/", (req, res) => {
-    let item = req.body.newItem;
-    items.push(item);
+    let newItemName = req.body.newItem;
+    const item = new Item({
+        name: newItemName
+    });
+    item.save();
     res.redirect("/");
+});
+
+app.post("/delete", (req, res) => {
+    const checkedItemId = req.body.id;
+    Item.findByIdAndRemove(checkedItemId, e => {
+        if (!e) {
+            console.log("Successfully deleted");
+        }
+        res.redirect("/");
+    })
 });
 
 
