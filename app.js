8429d326e0e55514ce4849721344025ec9060278
Centeno Jose Antonio <jocecito182@gmail.com>
2021-09-28 23:26:42 -0300

    Add procfile and update ports

--- app.js
+++ app.js
@@ -85,6 +85,12 @@ app.post("/delete", (req, res) => {
 });
 
 
-app.listen(3000, () => {
-    console.log("Funcionando");
-});
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 3000;
+}
+app.listen(port);
+
+// app.listen(3000, () => {
+//     console.log("Funcionando");
+// });
